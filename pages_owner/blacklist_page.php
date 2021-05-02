eda1fc52bee92524a804da93575aa16ea8aa37a8
YC Pan <yp9ar@virginia.edu>
2021-05-03 07:47:05 +0800

    fixed bugs for blacklist fucntionality & functionable add functionality

--- pages_owner/blacklist_page.php
+++ pages_owner/blacklist_page.php
@@ -9,7 +9,7 @@
   <meta name="author" content="your name">
   <meta name="description" content="include some description about your page">
 
-  <title>My Projects</title>
+  <title>Blacklist users</title>
 
   <!-- bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
@@ -17,7 +17,7 @@
 
 
   <style>
-    <?php include '../stylesheets/owner/my_projects.css'; ?>
+    <?php include '../stylesheets/owner/blacklist_page.css'; ?>
   </style>
 </head>
 
@@ -41,7 +41,7 @@
             <a class="nav-link" href="#">My Projects <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="./create_page.php">Create New Projects <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="./createpj_page.php">Create New Projects  <span class="sr-only">(current)</span> </a>
           </li>
         </ul>
       <div class="btn-group">
@@ -67,9 +67,83 @@
     </nav>
 </header>
 
-  <div class="container">
-    
+
+<div class="container">
     <?php
+        include_once("../php/library.php"); // To connect to the database
+
+        $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+        // Check connection
+        if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        //list out users
+        $result = mysqli_query($con,"SELECT * FROM Joins WHERE projectID = 'ABCD1234'");
+
+        echo "<h3>Current Available Users</h3>";
+        echo "<table border='1' class='usertable' >
+        <tr>
+        <th>User ID</th>
+        <th>Project ID</th>
+        </tr>";
+
+        while($row = mysqli_fetch_array($result))
+        {
+        echo "<tr>";
+        echo "<td>" . $row['userID'] . "</td>";
+        echo "<td>" . $row['projectID'] . "</td>";
+        echo "</tr>";
+        }
+        echo "</table>";
+    
+        echo "<form action='../php/owner/blacklist.php' method='post' class='blacklistinput'> 
+        <input type='text' name='userID' placeholder='Enter wanted user's ID to blacklist'>
+        <input type='text' name='pjID' placeholder='Enter pj's ID to blacklist'>
+        <br/>
+        <button  type='submit' class='btn btn-primary' onclick='location.href='./blacklist_page.php';'>Bye</button>
+         </form>";
+
+         $result2 = mysqli_query($con,"SELECT * FROM Blacklist WHERE projectID = 'ABCD1234'");
+        
+        echo "<h3>Current Blacklisted Users</h3>";
+        echo "<table border='1' class='bltable' >
+        <tr>
+        <th>User ID</th>
+        <th>Project ID</th>
+        </tr>";
+
+        while($row2 = mysqli_fetch_array($result2))
+        {
+        echo "<tr>";
+        echo "<td>" . $row2['userID'] . "</td>";
+        echo "<td>" . $row2['projectID'] . "</td>";
+        echo "</tr>";
+        }
+        echo "</table>";
+
+        echo "<form action='../php/owner/blacklistback.php' method='post' class='blacklistinput'> 
+        <input type='text' name='userID' placeholder='Enter wanted user's ID to blacklist'>
+        <input type='text' name='pjID' placeholder='Enter pj's ID to blacklist'>
+        <br/>
+        <button  type='submit' class='btn btn-primary' onclick='location.href='./blacklist_page.php';'>Comeback</button>
+         </form>";
+
+        if (!mysqli_query($con,$result)) {
+            die('Error: ' . mysqli_error($con));
+        }  
+
+        if (!mysqli_query($con,$result2)) {
+          die('Error: ' . mysqli_error($con));
+      }  
+
+        
+
+        
+        mysqli_close($con);
+    ?>
+
+     <?php
       require_once('../php/library.php');
       $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
       // Check connection
@@ -81,12 +155,10 @@
       
       mysqli_close($con);
     ?>
-      
-
     
-      <h3>My Projects</h3>
+    <h3> header3</h3>
+    
 
-      
 
   </div>
 
--- php/owner/create_project.php
+++ php/owner/create_project.php
@@ -1,1 +1,42 @@
-<?php echo "this doesn't do anything yet!"?>
+<?php 
+    session_start();
+    if (!isset($_SESSION["ownerID"])){
+      $_SESSION["login_error_message"] = "Please login to continue.";
+      header("Location: ./landing_page.php");
+    }
+     ?>
+
+<?php
+ include_once("../library.php"); // To connect to the database
+
+ $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+ // Check connection
+ if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+$id = uniqid();
+
+ $sqlpj="INSERT INTO Project (projectID, project_name, max_participants, project_description) 
+ VALUES
+ ('$id','$_POST[pjname]','$_POST[max]','$_POST[pjtext]')";
+
+$sqlproposes="INSERT INTO Proposes (projectID,ownerID) 
+VALUES
+('$id','$_SESSION[ownerID]')";
+
+ 
+
+
+
+ if (!mysqli_query($con,$sqlpj)) {
+    die('Error: ' . mysqli_error($con));
+ }  
+ if (!mysqli_query($con,$sqlproposes)) {
+   die('Error: ' . mysqli_error($con));
+}  
+
+
+ 
+mysqli_close($con);
+?>
