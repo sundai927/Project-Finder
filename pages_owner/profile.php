b67c2154829446bb98b2fb5029f585543e087b71
sundai927 <47261284+sundai927@users.noreply.github.com>
2021-05-04 23:03:23 -0400

    Update profile.php

--- pages_owner/profile.php
+++ pages_owner/profile.php
@@ -44,12 +44,8 @@
     </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Main Feed <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="#">My Followed Projects <span class="sr-only">(current)</span></a>
-
+       <li class="nav-item active">
+            <a class="nav-link" href="./my_projects.php">My Projects <span class="sr-only">(current)</span></a>
           </li>
         </ul>
       <div class="btn-group">
